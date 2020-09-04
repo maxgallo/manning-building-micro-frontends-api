d93f84624b5c26a3c45aaf1efb62f6933644fbf7
Max Gallo <max.gallo@dazn.com>
2020-09-04 22:00:01 +0100

    change API to songs

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -92,7 +92,7 @@ resource "aws_api_gateway_integration" "post_validate_integration" {
 resource "aws_api_gateway_resource" "songs" {
   rest_api_id = aws_api_gateway_rest_api.building_mfe.id
   parent_id   = aws_api_gateway_resource.api.id
-  path_part   = "mymusic"
+  path_part   = "songs"
 }
 
 resource "aws_api_gateway_method" "get_songs" {
