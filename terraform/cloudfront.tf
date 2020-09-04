4222843cdbb22eead6968298bff4f9c499c07d2f
Max Gallo <max.gallo@dazn.com>
2020-09-04 20:12:11 +0100

    add POST to allowed methods in CF

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -32,8 +32,8 @@ resource "aws_cloudfront_distribution" "building_mfe" {
   price_class     = "PriceClass_100"
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    allowed_methods  = ["GET", "POST", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "POST", "HEAD", "OPTIONS"]
     target_origin_id = local.api_gateway_origin_id
 
     forwarded_values {
